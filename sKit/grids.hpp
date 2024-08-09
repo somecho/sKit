27066e4d894790ca366e5f231424f045a5928210
Some Cho <itssomicho@gmail.com>
2024-08-09 21:44:04 +0200

    document s::Kit::gridFillDiagonal

--- sKit/grids.hpp
+++ sKit/grids.hpp
@@ -7,7 +7,21 @@ namespace s {
 namespace Kit {
 
 /**
+ * @brief Creates a path following the coordinates of every cell in a grid that
+ * has a diagonal tendency.
+ *
+ * Example of a 3x3 grid and the path that fills it:
+ * ```
+ * ┌────┬────┬────┐
+ * │ 1  │ 2  │ 6  │
+ * ├────┼────┼────┤
+ * │ 3  │ 5  │ 7  │
+ * ├────┼────┼────┤
+ * │ 4  │ 8  │ 9  │
+ * └────┴────┴────┘
+ * ```
  * @throw std::runtime_error If either `numCols` or `numRows` are less than 2.
+ * @return Every point in a `numCols` by `numRows` grid.
  */
 auto gridFillDiagonal(int numCols, int numRows) -> std::vector<glm::ivec2>;
 
